cf9581106a5a67b51a20737b0ad9e329a767b576
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-04-16 17:13:05 +0200

    Cleaned Code a bit up

--- Main.asm
+++ Main.asm
@@ -1,64 +1,28 @@
 .386
-
 .model flat,stdcall
 .stack 4096
 ExitProcess proto,dwExitCode:dword
 
 include logic.inc
+include draw.inc
+include setup.inc
 
 .data
-
-
 .code
 main proc
-    push offset WinTitle
-    push 400
-    push 400
-    call InitWindow
-    add esp, 12
-
-    mov         esi,esp  
-    push        40h  
-    call        SetWindowState 
-    add         esp,4  
-
-    push 60
-    call SetTargetFPS
-    add esp, 4
+    call setup
     whilestart:
     call WindowShouldClose 
     movzx eax,al  
     test eax,eax  
     jne whileexit
         call logic
-        mov         esi,esp  
-        mov         eax,dword ptr ColBG  
-        push        eax
-        call        ClearBackground
-        add         esp,4    
-        call BeginDrawing
-
-        mov         esi,esp  
-        mov         eax,dword ptr ColREC
-        push        eax  
-
-        push        25  
-
-        push        25  
-        mov         eax,dword ptr sPlayer.y 
-        push        eax  
-        mov         ecx,dword ptr sPlayer.x 
-        push        ecx  
-        call        DrawRectangle
-        add         esp,14h  
-        call EndDrawing
+        call draw
     jmp whilestart
     whileexit:
 
     call CloseWindow
     invoke ExitProcess,0
-
-
 
 main endp
 end main
